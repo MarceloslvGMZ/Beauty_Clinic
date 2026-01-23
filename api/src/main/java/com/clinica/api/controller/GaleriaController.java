50dcbc4e339f0198f6ff798e8c42f3aa948d8784
marceloSlvGmz <marcelogmz.dev@gmail.com>
2026-01-22 21:51:09 -0300

    feat: Creating Get method

--- api/src/main/java/com/clinica/api/controller/GaleriaController.java
+++ api/src/main/java/com/clinica/api/controller/GaleriaController.java
@@ -2,17 +2,18 @@ package com.clinica.api.controller;
 
 import com.clinica.api.model.galeria.GaleriaPortfolio;
 import com.clinica.api.model.galeria.GaleriaRequestDTO;
-import com.clinica.api.model.servico.Servico;
+import com.clinica.api.model.galeria.GaleriaResponseDTO;
 import com.clinica.api.service.GaleriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/servico/portfolio")
+@RequestMapping("/api/portfolio")
 public class GaleriaController {
 
     @Autowired
@@ -26,6 +27,14 @@ public class GaleriaController {
         GaleriaRequestDTO galeriaRequestDTO = new GaleriaRequestDTO(image, servicoId);
         GaleriaPortfolio galeria = this.galeriaService.criarPortfolio(galeriaRequestDTO);
 
+        return ResponseEntity.ok(galeria);
+    }
+
+    @GetMapping(path = "/servico/{servicoId}")
+    public ResponseEntity<List<GaleriaResponseDTO>> listarPortfolioPorServico(
+            @PathVariable UUID servicoId
+    ){
+        List<GaleriaResponseDTO> galeria = this.galeriaService.listarPortfolioPorServico(servicoId);
         return ResponseEntity.ok(galeria);
     }
 }
