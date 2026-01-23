997bb6c6d12f1e8e47325c074627ad49c9999346
marceloSlvGmz <marcelogmz.dev@gmail.com>
2026-01-22 21:54:01 -0300

    config: configuring repository with new function

--- api/src/main/java/com/clinica/api/repository/GaleriaRepository.java
+++ api/src/main/java/com/clinica/api/repository/GaleriaRepository.java
@@ -1,9 +1,12 @@
 package com.clinica.api.repository;
 
 import com.clinica.api.model.galeria.GaleriaPortfolio;
+import com.clinica.api.model.galeria.GaleriaResponseDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface GaleriaRepository extends JpaRepository<GaleriaPortfolio, UUID> {
+    List<GaleriaPortfolio> findByServicoId(UUID servicoId);
 }
